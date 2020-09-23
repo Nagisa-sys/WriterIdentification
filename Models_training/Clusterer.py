5e942fd783d5847a52b7d0f93ce795427a66355e
Nagisa-sys <hicham.dahmou@uit.ac.ma>
2020-09-23 08:25:47 +0100

    More speed for the clustering step

--- Models_training/Clusterer.py
+++ Models_training/Clusterer.py
@@ -8,12 +8,10 @@ class Clusterer:
 		return np.load(path_centers)
 
 	@staticmethod
-	def fit_new_trainig(images, path_to_save=None, nb_clusters=300, max_no_improvement=1000, verbose=1):
-		descriptor_list = []
-		[descriptor_list.extend(image.local_descriptors) for image in images]
-		np.random.shuffle(descriptor_list)
+	def fit_new_trainig(vectors, path_to_save=None, nb_clusters=300, max_no_improvement=1000, verbose=1, shuffle=True):
+		if shuffle: np.random.shuffle(vectors)
 		kmeans = MiniBatchKMeans(n_clusters = nb_clusters, verbose=verbose, max_no_improvement=max_no_improvement)
-		kmeans.fit(descriptor_list)
+		kmeans.fit(vectors)
 
 		if path_to_save is not None:
 			np.save(path_to_save, kmeans.cluster_centers_)
@@ -21,10 +19,11 @@ class Clusterer:
 		return kmeans
 
 	@staticmethod
-	def elbow_method_kmeans(images, max_no_improvement, test_values ,figsize=(15, 5), verbose=0):
+	def elbow_method_kmeans(vectors, max_no_improvement, test_values ,figsize=(15, 5), verbose=0):
 		from matplotlib import pyplot as plt
-
-		distorsions = [Clusterer.fit_new_trainig(images, nb_clusters=k, max_no_improvement=max_no_improvement, verbose=verbose).inertia_ for k in test_values]
+        
+		np.random.shuffle(vectors)
+		distorsions = [Clusterer.fit_new_trainig(vectors, nb_clusters=k, max_no_improvement=max_no_improvement, verbose=verbose, shuffle=False).inertia_ for k in test_values]
 		
 		fig = plt.figure(figsize=figsize)
 		plt.plot(test_values, distorsions, marker='o')
